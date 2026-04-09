071121727e1c491a41aa84a4611da0a4f8adf9e9
Ayar Kyaw <rooney.ayarkyaw@gmail.com>
2026-04-09 21:41:08 +0630

    Changed Migration

--- database/migrations/2026_04_06_041833_add_prices_to_event_ticket_types_table.php
+++ database/migrations/2026_04_06_041833_add_prices_to_event_ticket_types_table.php
@@ -10,12 +10,16 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up()
-{
-    Schema::table('event_ticket_types', function (Blueprint $table) {
-        $table->decimal('national_price', 10, 2)->after('type')->default(0);
-        $table->decimal('foreign_price', 10, 2)->after('national_price')->default(0);
-    });
-}
+    {
+        Schema::table('event_ticket_types', function (Blueprint $table) {
+            if (!Schema::hasColumn('event_ticket_types', 'national_price')) {
+                $table->decimal('national_price', 10, 2)->after('type')->default(0);
+            }
+            if (!Schema::hasColumn('event_ticket_types', 'foreign_price')) {
+                $table->decimal('foreign_price', 10, 2)->after('national_price')->default(0);
+            }
+        });
+    }
 
 public function down()
 {
