29eedc2d3aa93df4063182d863cc9e668d199299
Ayar Kyaw <rooney.ayarkyaw@gmail.com>
2026-04-17 20:44:10 +0630

    Print

--- resources/views/dashboard/ticket-sales/ticket.blade.php
+++ resources/views/dashboard/ticket-sales/ticket.blade.php
@@ -85,10 +85,10 @@
     .print-active {
         position: fixed;
 
-        top: 50%;
+        top: 60%;
         left: 50%;
 
-        transform: translate(-50%, -50%) scale(1.4);
+        transform: translate(-50%, -50%) scale(1.8);
         transform-origin: center;
 
         width: 80mm !important;
